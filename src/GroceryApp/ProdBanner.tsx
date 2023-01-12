110d32c1bc1f0786705b2c28be47e4ad7d22cb72
Rita-cedcoss <ritapal@cedcoss.com>
2023-01-12 18:17:16 +0530

    task completed grocery app

--- src/GroceryApp/ProdBanner.tsx
+++ src/GroceryApp/ProdBanner.tsx
@@ -5,7 +5,7 @@ type  prodbannerProps={
 }
 const ProdBanner = (props:prodbannerProps) => {
   return (
-    <div className='container-fluid mt-2 mb-2'>
+    <div className='container-fluid mt-4 p-4 mb-2'>
        <img alt='' src={props.imgs} style={{"minHeight":`${props.height}`,"width":"100%"}}/>
     </div>
   )
